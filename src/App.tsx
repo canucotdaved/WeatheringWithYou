ed386db7b6684a0178fa9e644b29a0112aa1d0b9
DNA-Dev <dna-dev@pop-os.localdomain>
2022-04-06 15:02:20 +0800

    added api and gotissue

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useMemo } from "react";
 import DaysForecast from "./components/DaysForecast";
 import PlaceInfo from "./components/PlaceInfo";
 import WeatherInfo from "./components/WeatherInfo";
+import { format } from "date-fns";
+const axios = require("axios");
 
 const ApiKey = process.env.REACT_APP_GMAP_API_KEY;
 const mapApiJs = "https://maps.googleapis.com/maps/api/js";
+
+let autocomplete: any;
 
 const loadAsyncScript = (src: any) => {
   return new Promise((resolve) => {
@@ -19,37 +23,57 @@ const loadAsyncScript = (src: any) => {
   });
 };
 
+const initMapScript = () => {
+  if (window.google) {
+    return Promise.resolve();
+  }
+  const src = `${mapApiJs}?key=${ApiKey}&libraries=places&v=weekly`;
+  return loadAsyncScript(src);
+};
+
+const initAutocomplete = (updateCity: any, searchInput: any) => {
+  autocomplete = new window.google.maps.places.Autocomplete(
+    searchInput.current,
+    { types: ["(cities)"] }
+  );
+  autocomplete.setFields(["address_component", "formatted_address"]);
+  autocomplete.addListener("place_changed", () =>
+    handlePlaceSelect(updateCity)
+  );
+};
+
+async function handlePlaceSelect(updateCity: any) {
+  const addressObject = autocomplete.getPlace();
+  const city = addressObject.formatted_address;
+  updateCity(city);
+}
+
 function App() {
-  const initMapScript = () => {
-    if (window.google) {
-      return Promise.resolve();
-    }
-    const src = `${mapApiJs}?key=${ApiKey}&libraries=places&v=weekly`;
-    return loadAsyncScript(src);
-  };
+  const searchInput = useRef(null);
+  // const memoized = useMemo(searchInput => );
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState([]);
+  console.log(city);
+
+  const today = format(new Date(), "E, dd MMM");
+
+  const weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`;
+
+  if (!!city)
+    axios.get(weatherApi).then((res: any) => {
+      console.log(1);
+      console.log(res.data);
+      console.log(searchInput);
+    });
 
   useEffect(() => {
-    initMapScript().then(() => initAutocomplete());
-  }, []);
+    initMapScript().then(() => initAutocomplete(setCity, searchInput));
+  }, [city]);
 
-  const initAutocomplete = () => {
-    if (!searchInput.current) return;
-
-    const autocomplete = new window.google.maps.places.Autocomplete(
-      searchInput.current
-    );
-    autocomplete.setFields(["address_component", "geometry"]);
-  };
-
-  const searchInput = useRef(null);
-  const [city, setCity] = useState("");
-  const inputHandler = (e: any) => {
-    setCity(e.target.value);
-  };
   return (
     <div className="bg-main bg-cover bg-no-repeat bg-center h-[100vh] flex">
       <div className="p-10 w-full md:w-1/2">
-        <PlaceInfo temperature="23" dateTime="Tue, 05 Apr" place={city} />
+        <PlaceInfo temperature={weatherData} dateTime={today} place={city} />
         <DaysForecast />
         <WeatherInfo />
       </div>
--- src/components/PlaceInfo.tsx
+++ src/components/PlaceInfo.tsx
@@ -9,12 +9,16 @@ interface PropType {
 const PlaceInfo: React.FC<PropType> = ({ temperature, dateTime, place }) => {
   return (
     <div className="flex flex-row items-center">
-      <h2 className="font-Poppins font-bold text-8xl pr-5">
-        {temperature}&deg;
-      </h2>
+      {temperature?.main?.temp ? (
+        <h2 className="font-Poppins font-bold text-8xl pr-5">
+          {temperature?.main?.temp}&deg;
+        </h2>
+      ) : null}
       <div className="flex flex-col">
         <p className="font-Poppins font-extralight text-2xl">{dateTime}</p>
-        <p className="font-Poppins font-normal text-4xl">{place}</p>
+        <p className="font-Poppins font-normal text-4xl">
+          {!place ? "Please Select City" : place}
+        </p>
       </div>
     </div>
   );
