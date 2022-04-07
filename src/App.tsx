d1821bb0c0e611d79a0a09b0f1ed82ca8c4fff54
DNA-Dev <dna-dev@pop-os.localdomain>
2022-04-07 15:06:41 +0800

    images map not working

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const mapApiJs = "https://maps.googleapis.com/maps/api/js";
 
 let autocomplete: any;
 
-const loadAsyncScript = (src: any) => {
+const loadAsyncScript = (src: string) => {
   return new Promise((resolve) => {
     const script = document.createElement("script");
     Object.assign(script, {
@@ -27,48 +27,69 @@ const initMapScript = () => {
   if (window.google) {
     return Promise.resolve();
   }
-  const src = `${mapApiJs}?key=${ApiKey}&libraries=places&v=weekly`;
+  const src = `${mapApiJs}?key=${ApiKey}&libraries=places`;
   return loadAsyncScript(src);
 };
 
-const initAutocomplete = (updateCity: any, searchInput: any) => {
+const initAutocomplete = (updateCity: any, searchInput: any, setPhoto: any) => {
   autocomplete = new window.google.maps.places.Autocomplete(
     searchInput.current,
     { types: ["(cities)"] }
   );
-  autocomplete.setFields(["address_component", "formatted_address"]);
+  autocomplete.setFields(["name", "photo"]);
   autocomplete.addListener("place_changed", () =>
-    handlePlaceSelect(updateCity)
+    handlePlaceSelect(updateCity, setPhoto)
   );
 };
 
-async function handlePlaceSelect(updateCity: any) {
+async function handlePlaceSelect(updateCity: any, setPhoto: any) {
   const addressObject = autocomplete.getPlace();
-  const city = addressObject.formatted_address;
+  const city = addressObject.name;
+  // const image = addressObject.photos[0].getUrl({
+  //   maxWidth: 1920,
+  //   maxHeight: 1080,
+  // });
+  const arr: any = [];
+  addressObject.photos.map((url: any) => {
+    const img = url.getUrl({ maxWidth: 1920, maxHeight: 1080 });
+    arr.push({ img });
+  });
   updateCity(city);
+  setPhoto(arr);
 }
 
 function App() {
   const searchInput = useRef(null);
-  // const memoized = useMemo(searchInput => );
   const [city, setCity] = useState("");
+  const [mainCity, setMainCity] = useState("");
+  const [photo, setPhoto] = useState([]);
   const [weatherData, setWeatherData] = useState([]);
-  console.log(city);
-
   const today = format(new Date(), "E, dd MMM");
 
-  const weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`;
+  const weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`;
 
-  if (!!city)
-    axios.get(weatherApi).then((res: any) => {
-      console.log(1);
-      console.log(res.data);
-      console.log(searchInput);
-    });
-
+  const fetchData = async () => {
+    try {
+      if (!city) return;
+      const response = await axios.get(weatherApi);
+      setWeatherData(response.data);
+      console.log(response.data);
+    } catch (err: any) {
+      if (err.response) {
+        console.log(err.response.data);
+        console.log(err.response.status);
+        console.log(err.response.headers);
+      } else {
+        console.log(`Error: ${err.message}`);
+      }
+    }
+  };
   useEffect(() => {
-    initMapScript().then(() => initAutocomplete(setCity, searchInput));
-  }, [city]);
+    initMapScript().then(() =>
+      initAutocomplete(setCity, searchInput, setPhoto)
+    );
+    fetchData();
+  }, [searchInput]);
 
   return (
     <div className="bg-main bg-cover bg-no-repeat bg-center h-[100vh] flex">
@@ -77,13 +98,21 @@ function App() {
         <DaysForecast />
         <WeatherInfo />
       </div>
-      <div className="p-10 w-full md:w-1/2">
+      <div className="p-10 w-full md:w-1/2 flex flex-col">
         <input
           type="text"
-          className="border-b bg-transparent placeholder:font-Poppins text-xl focus:ring-[1px] font-Poppins font-light pl-5"
+          className="border-b bg-transparent placeholder:font-Poppins text-xl focus:ring-0 font-Poppins font-light pl-5"
           placeholder="Enter City"
           ref={searchInput}
         />
+        <br />
+        {!city ? null : (
+          <h3 className="text-center font-Poppins">Some photos of {city} </h3>
+        )}
+        <br />
+        {photo.map(({ img }: any) => {
+          <p>{img}</p>;
+        })}
       </div>
     </div>
   );
--- src/components/PlaceInfo.tsx
+++ src/components/PlaceInfo.tsx
@@ -11,9 +11,11 @@ const PlaceInfo: React.FC<PropType> = ({ temperature, dateTime, place }) => {
     <div className="flex flex-row items-center">
       {temperature?.main?.temp ? (
         <h2 className="font-Poppins font-bold text-8xl pr-5">
-          {temperature?.main?.temp}&deg;
+          {Math.round(temperature?.main?.temp)}&deg;
         </h2>
-      ) : null}
+      ) : (
+        <h2 className="font-Poppins font-bold text-8xl pr-5">00&deg;</h2>
+      )}
       <div className="flex flex-col">
         <p className="font-Poppins font-extralight text-2xl">{dateTime}</p>
         <p className="font-Poppins font-normal text-4xl">
